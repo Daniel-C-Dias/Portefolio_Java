3f859826d6cae59ecaf9da3ce9e460e08d136cfe
gonca <gonca@DESKTOP-OO46CQA>
2017-05-01 21:15:03 +0100

    Criacao classe Centro de Eventos e interface AlgoritmoAtribuicao

---
+++ PPROG_1ND_1130182_1160089_TP3/src/TP3/CentroEventos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP3;
+
+import java.util.List;
+
+/**
+ *
+ * @author gonca
+ */
+public class CentroEventos {
+    
+    public List<Evento> getRegistoEventos(){
+       
+    }
+    
+    public List<Evento> getRegistoUtilizadores(){
+       
+    }
+}
--- PPROG_1ND_1130182_1160089_TP3/src/TP3/Utilizador.java
+++ PPROG_1ND_1130182_1160089_TP3/src/TP3/Utilizador.java
@@ -5,6 +5,9 @@
  */
 package TP3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author gonca
@@ -14,5 +17,16 @@ public class Utilizador {
     private String userName;
     private String email;
     private String password;
+    private String nome;
+    
+    public enum Role{ 
+       Gestor_Eventos{ public String toString() { return "Gestor de Eventos"; }},
+       Organizador{ public String toString() { return "Organizador"; }},
+       FAE { public String toString() { return "Funcionário de Apoio ao Evento"; }},
+       Participante{ public String toString() { return "Participante"; }}}  
+    
+    
+    List <Role> rolesUtilizador = new ArrayList();
+    
     
 }
