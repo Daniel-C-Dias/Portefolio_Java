b067e5d09b52740ff860f2a7562832218e4b52f2
Daniel Dias <1130182@isep.ipp.pt>
2017-05-08 23:31:32 +0100

    Criação Classe Main e alterações a RegistarCandidaturaUI

---
+++ PPROG_1ND_1130182_1160089_TP3/src/Index/Main.java
@@ -0,0 +1,16 @@
+
+package Index;
+
+import UI.*;
+
+/**
+ *
+ * @author Daniel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        RegistarCandidaturaUI rc = new RegistarCandidaturaUI();
+    }
+
+}
